3882382e84a656f4ebe912cdaeb76e6775f68669
Nilufar Hatamova <hatamovanilufar@gmail.com>
2020-08-26 21:28:37 +0200

    adding pom and other source files

---
+++ src/main/java/com/jdoclint/plugin /properties/Properties.java
@@ -0,0 +1,15 @@
+package com.jdoclint.plugin.properties; 
+
+public class Properties {
+    public String getLineLength() {
+        return null; 
+    }
+
+    public boolean isSpellCheckEnabled() {
+        return false; 
+    }
+
+    public boolean isGrammarCheckEnabled() {
+        return false; 
+    }
+}
